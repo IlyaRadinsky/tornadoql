de460c7660c14b033ab5102ad4efbad9f345ad8b
IlyaRadinsky <ilya_radinsky@hotmail.com>
2017-11-09 12:34:15 +0300

    Added a sample of mutation

--- schema.py
+++ schema.py
@@ -1,9 +1,37 @@
 import graphene
+import uuid
+
+class Message(graphene.ObjectType):
+    id = graphene.ID()
+    userId = graphene.ID()
+    msg = graphene.String()
+
+
+class PostMutation(graphene.Mutation):
+    class Arguments:
+        userId = graphene.ID(required=True)
+        msg = graphene.String(required=True)
+
+    Output = Message
+
+    def mutate(self, info, userId, msg):
+        msg = Message(
+            id=str(uuid.uuid4()),
+            userId=userId,
+            msg=msg
+        )
+        return msg
+
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
 
-    def resolve_hello(self, args):
+    def resolve_hello(self, info):
         return 'Hello from GraphQL!'
 
-schema = graphene.Schema(query=Query)
+
+class Mutation(graphene.ObjectType):
+    post = PostMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
